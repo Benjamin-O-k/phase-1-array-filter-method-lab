32dfb3eb0845d872db74de5456fe23c9736719e7
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-07-03 17:27:44 +0300

    passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, string){
+    return drivers.filter(drivers => {return drivers.toLowerCase() == string.toLowerCase()})
+}
+
+console.log(findMatching(drivers , "bobby"))
+function fuzzyMatch(drivers, string){
+    return drivers.filter(drivers =>{ return drivers.toLowerCase().startsWith(string.toLowerCase()) === string.toLowerCase().startsWith(string.toLowerCase())})
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+}
+
+
